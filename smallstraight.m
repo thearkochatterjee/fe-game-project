ec7cd0344e42b7f52c0d2aa64d21d2978833db1a
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-07 12:03:29 -0500

    Update smallstraight.m

--- smallstraight.m
+++ smallstraight.m
@@ -1,6 +1,14 @@
 function [straight] = smallstraight(hand)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-
+for i = lowestvalue(hand):1:(lowestvalue(hand)+3)
+    has = false;
+    if isinhand(hand,i) == true
+        has = true;
+    else
+        break;
+    end
+end
+straight = has;
 end
 
