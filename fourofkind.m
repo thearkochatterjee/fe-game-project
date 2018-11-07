d1c2bafcbaf0c72e0750f40eb89acf9eba9488fb
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-07 08:58:17 -0500

    Adding function for detecting if four of a kind is present

---
+++ fourofkind.m
@@ -0,0 +1,23 @@
+function [kind] = fourofkind(hand)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+largestnummatch = 0;
+for x=1:1:6
+    count = 1;
+    for y=2:1:6
+        if hand(y) == hand(x)
+            count = count + 1;
+        end
+    end
+    if count > largestnummatch
+       largestnummatch=count; 
+    end
+end
+largestnummatch
+if largestnummatch == 4
+    kind = true;
+else
+    kind = false;
+end
+end
+
