34b8e78d1f4c17b6fa2b7ac2c85ee7d2f69b3151
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-08 16:41:46 -0500

    Update largestraight.m

--- largestraight.m
+++ largestraight.m
@@ -1,6 +1,15 @@
 function [straight] = largestraight(hand)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
-
+for i = lowestvalue(hand):1:(lowestvalue(hand)+4)
+    has = false;
+    if isinhand(hand,i) == true
+        has = true;
+    else
+        has = false;
+        break;
+    end
+end
+straight = has;
 end
 
