d83389d0cc72de5b3ef9bf1b624a8ab5b8d54c1f
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-08 16:42:08 -0500

    Update Yathzee.m

--- Yathzee.m
+++ Yathzee.m
@@ -5,18 +5,19 @@ ai=input('Would you like to include the computer in this game (y - yes, n - no)'
 if ai=='y'
     fprintf('with computer')
 else
-    dice_results=randi([1,6],1,5)
-    more = 'y';
-    i=1;
-    keep=[];
-    while more=='y'
-        keep(i) = dice_results(input('What number dice would you like to keep?'));
-        i=i+1;
-        more = input('Are there any other dice that you would like to keep?','s');
-    end
-    keep
-    for i = length(keep):1:5
-       keep(i)=randi([1,6]);
-    end
-    keep
+    
+%     dice_results=randi([1,6],1,5)
+%     more = 'y';
+%     i=1;
+%     keep=[];
+%     while more=='y'
+%         keep(i) = dice_results(input('What number dice would you like to keep?'));
+%         i=i+1;
+%         more = input('Are there any other dice that you would like to keep?','s');
+%     end
+%     keep
+%     for i = length(keep):1:5
+%        keep(i)=randi([1,6]);
+%     end
+%     keep
 end
