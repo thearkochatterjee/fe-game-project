4bbff93025c937c09bab442407bb29ed3e670830
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-08 16:41:36 -0500

    Update fullhouse.m

--- fullhouse.m
+++ fullhouse.m
@@ -1,24 +1,18 @@
 function [fullhouse] = fullhouse(hand)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
-if threeofkind(hand)==true || fourofkind(hand)==true
-   largestnummatch = 5;
-for x=1:1:5
-    count = 1;
-    for y=x:1:5
-        if hand(y) == hand(x)
+count = 0;
+if threeofkind(hand) == true && fourofkind == false
+    for i = 1:1:length(hand)
+        if isinhand(hand,i)
             count = count + 1;
         end
     end
-    if count < largestnummatch
-       largestnummatch=count; 
-    end
 end
-if largestnummatch == 2
+if count == 2
     fullhouse = true;
 else
     fullhouse = false;
-end 
 end
 end
 
