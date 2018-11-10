c14fc918c932ea8d16d93cfefc18fa97f538b578
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 16:22:17 -0500

    Update yahtzee/threeofkind.m

--- yahtzee/threeofkind.m
+++ yahtzee/threeofkind.m
@@ -1,19 +1,24 @@
 function [kind] = threeofkind(hand)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-largestnummatch = 0;
-for x=1:1:5
-    count = 1;
-    for y=x:1:5
-        if hand(y) == hand(x)
-            count = count + 1;
+%threeofkind detects if there are three matching dice rolls in the player's
+% hand
+
+% Intializing the count variable for each value of a possible dice roll
+for x = 1:1:6
+    count(x) = 0;
+end
+
+% Loop through each possible outcome for a dice roll
+for v = 1:1:6
+    % Loop through all values in the hand and count the occurrange of v
+    for i = 1:1:length(hand)
+        if hand(i) == v
+            count(v) = count(v) + 1;
         end
     end
-    if count > largestnummatch
-       largestnummatch=count; 
-    end
 end
-if largestnummatch == 3
+% Test if the highest number of matches is at least 3 to see if it is
+% possible to declare that there is a three of a kind present in the hand
+if highestvalue(count) >= 3
     kind = true;
 else
     kind = false;
