d9a837e59d23416f6ec8ecd2d62bf8f690febb20
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 16:22:23 -0500

    Update yahtzee/yahtzeedetection.m

--- yahtzee/yahtzeedetection.m
+++ yahtzee/yahtzeedetection.m
@@ -1,6 +1,5 @@
 function [hasyahtzee] = yahtzeedetection(hand)
-%UNTITLED8 Summary of this function goes here
-%   Detailed explanation goes here
+%yahtzeedetecttion detects if all the dice in the hand are the same value
 if hand(1)==hand(2) && hand(2) == hand(3) && hand(3) == hand(4) && hand(4) == hand(5)
     hasyahtzee=true;
 else
