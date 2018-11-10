a0e12c400d61ee31cf874fd930136814a3bfe325
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 16:21:58 -0500

    Update yahtzee/fullhouse.m

--- yahtzee/fullhouse.m
+++ yahtzee/fullhouse.m
@@ -1,14 +1,22 @@
 function [fullhouse] = fullhouse(hand)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+%full house detects if there is a three of a kind and a pair present in the
+% hand
+
+%intialize the count for detecting if there is a pair
 count = 0;
+% Testing if there is a three of a kind but not a four of a kind so it is
+% possible for a pair to be present
 if threeofkind(hand) == true && fourofkind == false
+    % Loops through all elements of hand array
     for i = 1:1:length(hand)
+        % Test if value tested is present in the hand and counts the
+        % occurances
         if isinhand(hand,i)
             count = count + 1;
         end
     end
 end
+% Output results for if there is a full house or not
 if count == 2
     fullhouse = true;
 else
