5df7e4cc050581b262551d4ef2ef6f00b1e9f00f
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 16:22:11 -0500

    Update yahtzee/lowestvalue.m

--- yahtzee/lowestvalue.m
+++ yahtzee/lowestvalue.m
@@ -1,12 +1,19 @@
 function [lowest] = lowestvalue(hand)
-%UNTITLED10 Summary of this function goes here
-%   Detailed explanation goes here
+%lowestvalue finds the lowest value of all the dice rolled in the hand
+
+% Intialize the low value to high value so none of the possible values are
+% excluded from being the lowest
 low = 7;
+% Loop through all values rolled present in the hand
 for i = 1:1:length(hand)
+    % Compare the current value being checked to the current lowest and if
+    % it is lower than the lowest value will be adjusted
     if hand(i) < low
         low = hand(i);
     end
 end
+% Sets the lowest value in the hand to the low after the loop as gone
+% through all the dice rolled
 lowest = low;
 end
 
