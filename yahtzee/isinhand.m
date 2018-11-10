9db2a50c2389272a60552f382014d524ab26e8af
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 16:22:04 -0500

    Update yahtzee/isinhand.m

--- yahtzee/isinhand.m
+++ yahtzee/isinhand.m
@@ -1,14 +1,17 @@
 function [contains] = isinhand(hand,value)
 %isinhand - detects if a specific value is in the array hand
 
-
+% Intialize detection variable to false
 con = false;
+% Loop through all element of the hand array
 for i = 1:1:length(hand)
+    % Detects if the value is present in the hand
     if hand(i) == value
         con = true;
         break;
     end
 end
+% Output results for if the value tested is anywhere in the array or not
 contains = con;
 end
 
