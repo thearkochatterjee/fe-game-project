e6a4aca44c9753420401b99d5b2265f3154d296f
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 16:21:55 -0500

    Update yahtzee/fourofkind.m

--- yahtzee/fourofkind.m
+++ yahtzee/fourofkind.m
@@ -1,23 +1,27 @@
 function [kind] = fourofkind(hand)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-largestnummatch = 0;
-for x=1:1:5
-    count = 1;
-    for y=x:1:5
-        if hand(y) == hand(x)
-            count = count + 1;
+%fourofkind detects if 4 of the 5 dice match
+
+% Intializing the count variable for each value of a possible dice roll
+for x = 1:1:6
+    count(x) = 0;
+end
+
+% Loop through each possible outcome for a dice roll
+for v = 1:1:6
+    % Loop through all values in the hand and count the occurrange of v
+    for i = 1:1:length(hand)
+        if hand(i) == v
+            count(v) = count(v) + 1;
         end
     end
-    if count > largestnummatch
-       largestnummatch=count; 
-    end
 end
-if largestnummatch == 4
+% Test if the highest number of matches is at least 3 to see if it is
+% possible to declare that there is a three of a kind present in the hand
+if highestvalue(count) >= 4
     kind = true;
 else
     kind = false;
 end
-endn
+end
 %luv github =- gaven
 
