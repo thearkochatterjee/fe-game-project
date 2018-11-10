8897646c4629774311387c3fcd6f38754eb0dbb5
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-10 17:04:45 -0500

    Update yahtzee/turn.m

--- yahtzee/turn.m
+++ yahtzee/turn.m
@@ -1,13 +1,24 @@
-function [roundscore] = turn(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-more = 'y';
+function [roundscore, category] = turn()
+%turn plays the players turn for a round and returns the score that they
+%achieved
+
+% Intialize variables for operation of the game
 rollagain = 'y';
-dice_results=randi([1,6],1,5)
+
+% Intial roll results
+dice_results = randi([1,6],1,5)
+
+% Loop until the player does not want to roll again
 while rollagain=='y'
     i=1;
-    rollagain=input('Do you want to roll again');
+
+    % Ask if the player wants to roll again
+    rollagain=input('Do you want to roll again?\n','s');
+    more = 'y';
     if rollagain=='y'
+        
+        % Loop through asking for asking all the dice that the player wants
+        % to keep
         while more=='y'
             keep(i) = dice_results(input('Which rolls do you want to keep?\n'));
             i=i+1;
@@ -17,11 +28,18 @@ while rollagain=='y'
         for x = l:1:5
             keep(x) = randi([1,6]);
         end
+        keep
         dice_results=keep;
     end
 end
+
+% Asks play what category to place the score into
 scorecategory=input('What category do you want these points to count for?','s');
+
+% Intialize the score for the round
 roundscore = 0;
+
+% Calculates the score for the intended category
 if scorecategory == 'aces'
     roundscore = uppertotal(dice_results,1);
 elseif scorecategory == 'twos'
@@ -47,9 +65,13 @@ elseif scorecategory == '4 of a kind'
         end
     end
 elseif scorecategory == 'small straight'
-    
+    if smallstraight(dice_results) == true
+        roundscore = 30;
+    end
 elseif scorecategory == 'large straight'
-    
+    if largestraight(dice_results) == true
+        roundscore = 40;
+    end
 elseif scorecategory == 'yahtzee'
     if yahtzeedetection(dice_results) == true
         roundscore = roundscore + 50;
@@ -59,5 +81,8 @@ elseif scorecategory == 'chance'
         roundscore = roundscore + dice_results(i);
     end
 end
+
+% Set the category used for output
+category = scorecategory;
 end
 
