ba55a268327369af0cb75e19e4984d70445a1414
Arko Chatterjee <arkochatterjee1006@gmail.com>
2018-11-11 20:58:35 -0500

    Update yahtzee/turn.m

--- yahtzee/turn.m
+++ yahtzee/turn.m
@@ -1,88 +1,47 @@
-function [roundscore, category] = turn()
+function [result] = turn()
 %turn plays the players turn for a round and returns the score that they
 %achieved
 
 % Intialize variables for operation of the game
 rollagain = 'y';
+rollcount = 1;
+load('Dice.mat');
 
 % Intial roll results
-dice_results = randi([1,6],1,5)
+dice_results = randi([1,6],1,5);
+imshow([Dice{dice_results}]);
 
 % Loop until the player does not want to roll again
-while rollagain=='y'
-    i=1;
+while rollagain=='y' && rollcount < 4
 
     % Ask if the player wants to roll again
     rollagain=input('Do you want to roll again?\n','s');
-    more = 'y';
     if rollagain=='y'
-        
+        rollcount = rollcount + 1;
         % Loop through asking for asking all the dice that the player wants
         % to keep
-        while more=='y'
-            keep(i) = dice_results(input('Which rolls do you want to keep?\n'));
-            i=i+1;
-            more = input('Do you want to keep anymore rolls?','s');
+        keep = dice_results;
+        while 1
+            ridof = input('Which dice do you want to roll again? (enter 0 to exit)\n');
+            if ridof == 0
+                break;
+            elseif ridof == 1 || ridof == 2 || ridof == 3 || ridof == 4 || ridof == 5
+                keep(ridof) = 0;
+            else
+                fprintf('Please use a valid input for which dice you want to roll again?\n');
+            end
         end
-        l = length(keep);
-        for x = l:1:5
-            keep(x) = randi([1,6]);
+        for y = 1:1:length(keep)
+            if keep(y) == 0
+                keep(y) = randi([1,6]);
+            end
         end
-        keep
         dice_results=keep;
+        close all;
+        imshow([Dice{dice_results}]);
     end
 end
 
-% Asks play what category to place the score into
-scorecategory=input('What category do you want these points to count for?','s');
-
-% Intialize the score for the round
-roundscore = 0;
-
-% Calculates the score for the intended category
-if scorecategory == 'aces'
-    roundscore = uppertotal(dice_results,1);
-elseif scorecategory == 'twos'
-    roundscore = uppertotal(dice_results,2);
-elseif scorecategory == 'threes'
-    roundscore = uppertotal(dice_results,3);
-elseif scorecategory == 'fours'
-    roundscore = uppertotal(dice_results,4);
-elseif scorecategory == 'fives'
-    roundscore = uppertotal(dice_results,5);
-elseif scorecategory == 'sixes'
-    roundscore = uppertotal(dice_results,6);
-elseif scorecategory == '3 of a kind'
-    if threeofkind(dice_results) == true
-        for i = 1:1:length(dice_results)
-            roundscore = roundscore + dice_results(i);
-        end
-    end
-elseif scorecategory == '4 of a kind'
-    if fourofkind(dice_results) == true
-        for i = 1:1:length(dice_results)
-            roundscore = roundscore + dice_results(i);
-        end
-    end
-elseif scorecategory == 'small straight'
-    if smallstraight(dice_results) == true
-        roundscore = 30;
-    end
-elseif scorecategory == 'large straight'
-    if largestraight(dice_results) == true
-        roundscore = 40;
-    end
-elseif scorecategory == 'yahtzee'
-    if yahtzeedetection(dice_results) == true
-        roundscore = roundscore + 50;
-    end
-elseif scorecategory == 'chance'
-    for i = 1:1:length(dice_results)
-        roundscore = roundscore + dice_results(i);
-    end
-end
-
-% Set the category used for output
-category = scorecategory;
+result = dice_results;
 end
 
